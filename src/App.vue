966872637e1bb072a7a4124d91c76db3f7a146ee
M.wait <18870742804@163.com>
2024-11-19 16:24:16 +0800

    导入定制化的样式文件和自动导入配置

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <template>
   <el-button type="primary">i am button</el-button>
+  <el-button type="primary">主按钮</el-button>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-// ElementPlus 配置自动按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers' 
-
-// https://vite.dev/config/
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    // 配置插件
-    AutoImport({
+    
+    AutoImport({	
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/var.scss" as *;
+        `,
+      }
+    }
+  }
 })
