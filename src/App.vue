f7491ef8e6b5d4f266b17454f1ef22b5fe0a3234
M.wait <18870742804@163.com>
2024-11-20 10:25:32 +0800

    吸顶导航交互实现

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import LayoutNav from '@/components/LayoutNav.vue'
 import LayoutHeader from '@/components/LayoutHead.vue'
 import LayoutFooter from '@/components/LayoutFooter.vue'
+import LayoutFixed from '@/components/LayoutFixed.vue'
 </script>
 
 <template>
+  <LayoutFixed/>
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
