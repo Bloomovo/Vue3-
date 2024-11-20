8b9b5bb4dfe87b4b1ab73d724c50e98537bbe160
M.wait <18870742804@163.com>
2024-11-20 08:18:28 +0800

    组件结构快速搭建

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-
+import LayoutNav from '@/components/LayoutNav.vue'
+import LayoutHeader from '@/components/LayoutHead.vue'
+import LayoutFooter from '@/components/LayoutFooter.vue'
 </script>
 
 <template>
-  <router-view></router-view>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
-
-<style scoped>
-</style>
