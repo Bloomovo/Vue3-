0481b9feaad46dfb755729bfb7d8c2632fe91006
M.wait <18870742804@163.com>
2024-11-21 13:37:58 +0800

    整体解构创建

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import HomeBanner from '@/components/HomeBanner.vue'
+import HomeCateGory from '@/components/HomeCateGory.vue'
+import HomeHot from '@/components/HomeHot.vue'
+import HomeNew from '@/components/HomeNew.vue'
+import HomeProduct from '@/components/HomeProduct.vue'
+</script>
+
 <template>
-  我是home
+  <div class="contianer">
+    <HomeCateGory/>
+    <HomeCateGory/>  
+  </div>
+    <HomeNew/>
+    <HomeHot/>
+    <HomeProduct />
 </template>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,3 +1,14 @@
+<script setup>
+import LayoutNav from '@/components/LayoutNav.vue'
+import LayoutHeader from '@/components/LayoutHead.vue'
+import LayoutFooter from '@/components/LayoutFooter.vue'
+import LayoutFixed from '@/components/LayoutFixed.vue'
+</script>
 <template>
+  <LayoutFixed/>
+  <LayoutNav />
+  <LayoutHeader />
   我是首页
+  <RouterView />
+  <LayoutFooter />
 </template>
