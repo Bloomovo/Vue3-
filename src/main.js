7adb0d7b2fade6a190efaa119039860890f22dd2
M.wait <18870742804@163.com>
2024-11-22 11:44:54 +0800

    懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import App from './App.vue'
 import router from './router'
 // 引入初始化样式
 import '@/styles/common.scss'
+// 引入懒加载
+import { lazyPlugin } from '@/directives/index.js'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
