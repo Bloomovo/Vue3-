c73435d528bdf5993d2dbfa2445fd6a3e53bc11f
M.wait <18870742804@163.com>
2024-11-22 09:34:42 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeCateGory from '@/components/HomeCateGory.vue'
 import HomeHot from '@/components/HomeHot.vue'
 import HomeNew from '@/components/HomeNew.vue'
 import HomeProduct from '@/components/HomeProduct.vue'
+import HomePanel from '@/components/HomePanel.vue'
 import { onMounted } from 'vue'
 import { useBannerStore } from '@/stores/banner.js'
 const bannerStore = useBannerStore()
@@ -20,4 +21,12 @@ onMounted(() => {
     <HomeNew/>
     <HomeHot/>
     <HomeProduct />
+    <HomePanel
+    title="新鲜好物"
+    sub-title="新鲜商品 好多商品"
+    ></HomePanel>
+    <HomePanel
+    title="人气推荐"
+    sub-title="人气推荐 好多商品"
+    ></HomePanel>
 </template>
